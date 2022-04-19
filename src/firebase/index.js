813a89ac923a079cd3ef9ff64a652695f1dc38f7
Alan-Figueredo <afigueredo2000@gmail.com>
2022-04-19 10:51:08 -0300

    cambios

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,16 +1,22 @@
-import firebase from "firebase/app"
-import "firebase/firestore"
 
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+  // Import the functions you need from the SDKs you need
+  import firebase from "firebase/app"
+  import "firebase/firestore"
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+  
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCjkoi72r85GwHez7P-vLr6T3EcXVZOtGk",
+    authDomain: "tusbebidasonline.firebaseapp.com",
+    projectId: "tusbebidasonline",
+    storageBucket: "tusbebidasonline.appspot.com",
+    messagingSenderId: "942541020743",
+    appId: "1:942541020743:web:df4700435da497471e5b62"
   };
-
-const app = firebase.initializeApp(firebaseConfig);
-
-  export const getFirebase =()=>app;
-  export const getFirestore = ()=>firebase.firestore(app);
+  
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  
+    export const getFirebase =()=>app;
+    export const getFirestore = ()=>firebase.firestore(app);
